05237e79c5753d4c42736959e342eefbd75afb85
Samuel Jacobs <samjacobs@pop-os.lan>
2024-06-11 07:25:13 -0400

    Minor updates.

--- CrayonVISOR/mainwindow.cpp
+++ CrayonVISOR/mainwindow.cpp
@@ -15,10 +15,10 @@ MainWindow::MainWindow(QWidget *parent)
     exitAct->setStatusTip(tr("Close Application"));
 
     QAction *aboutAct = ui->menuHelp->addAction(tr("&About CrayonVISOR..."), this, &MainWindow::About);
-    aboutAct->setStatusTip(tr("PlotVISOR is a visual assistant for interpreting contour plots..."));
+    aboutAct->setStatusTip(tr("CrayonVISOR is a visual assistant forb colors..."));
 
     QAction *aboutQtAct = ui->menuHelp->addAction(tr("About &Qt.."), qApp, &QApplication::aboutQt);
-    aboutQtAct->setStatusTip(tr("PlotVISOR was developed using Qt technologies..."));
+    aboutQtAct->setStatusTip(tr("CrayonVISOR was developed using Qt technologies..."));
 }
 
 MainWindow::~MainWindow()
@@ -29,9 +29,8 @@ MainWindow::~MainWindow()
 void MainWindow::About()
 {
     QMessageBox::about(this, tr("About Application"),
-                       tr("<b>PlotVISOR</b> is a visual assistant for contour plots.<br><br>"
-                          "By following the instructions (1) to (4) in the program window, users can map the legend of any contour plot and use it to identify values otherwise difficult to interpret.<br><br>"
-                          "If \"???\" is displayed instead of a valid result, disable lighting enhancements (within your host software) and/or remap the calibration curve. PlotVISOR has been configured to accommodate mild lighting variation from rendering, but this may affect results by ±1 legend increment.<br><br>"
+                       tr("<b>CrayonVISOR</b> is a visual assistant for colors.<br><br>"
+                          "Instructions: first, select the desired crayon box size (16, 48, etc.) from the dropdown list. Afterwards, click the button and drag your mouse anywhere on the screen. In real time, CrayonVISOR will interpret the color beneath the mouse and identify the best-matching crayon color from the selected box size.<br><br>"
                           "Please visit our <a href=\"https://www.scimulate.com\">website</a> and follow us on <a href=\"https://www.linkedin.com/company/scimulate-llc\">LinkedIn</a>!<br><br>"
-                          "PlotVISOR is made available to you under the terms of the <a href=\"https://www.gnu.org/licenses/gpl-3.0.html\">GPLv3</a>. You are not granted any trademark rights or licenses to the trademarks of Scimulate LLC.<br><br>"));
+                          "CrayonVISOR is made available to you under the terms of the <a href=\"https://www.gnu.org/licenses/gpl-3.0.html\">GPLv3</a>. You are not granted any trademark rights or licenses to the trademarks of Scimulate LLC.<br><br>"));
 }
